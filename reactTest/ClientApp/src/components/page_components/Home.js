8e92b613b4955de92fd376abdd326ca28dc95dd7
sornprasit <sornprasit.pr@hotmail.com>
2020-07-14 07:56:56 +0700

    Add Onclick and setstate

--- reactTest/ClientApp/src/components/page_components/Home.js
+++ reactTest/ClientApp/src/components/page_components/Home.js
@@ -1,9 +1,26 @@
 ﻿import React, { Component } from 'react';
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Text:'Hello Word!!'
+        }
+    }
+    onClickChange() {
+        this.setState({ Text: 'click' });
+    }
     render() {
         return (
             <React.Fragment>
-                Hello World!!
+                <canter>
+                    <div>
+                        {this.state.Text}
+                    </div>
+                    <button onClick={() => this.onClickChange()} >
+                        click
+                    </button>
+                </canter>
+
             </React.Fragment>            
             );
     }
