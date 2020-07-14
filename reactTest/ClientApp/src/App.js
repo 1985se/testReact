be409e80e13532260b4f3ccd2910574e033b82a8
sornprasit <sornprasit.pr@hotmail.com>
2020-07-14 07:47:17 +0700

    Hello Word

---
+++ reactTest/ClientApp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Home from './components/page_components/Home';
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+      return (
+          <switch>
+              <Route exact path='/' component={Home}/>
+          </switch>
+    );
+  }
+}
---
+++ reactTest/ClientApp/src/components/page_components/Home.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                Hello World!!
+            </React.Fragment>            
+            );
+    }
+
+}
+export default Home;
